38ab8295b89d6b2fe8ae8ac4607e3c73fc614c4f
Marcus <minh.nguyen@triona.fi>
2024-05-02 16:01:20 +0300

    fix lint

--- src/app/components/RelatedArticle.tsx
+++ src/app/components/RelatedArticle.tsx
@@ -1,5 +1,3 @@
-
-
 const RelatedArticle = () => {
   return <div>RelatedArticle</div>;
 };
--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -54,4 +54,3 @@ const CardFooter = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDiv
 CardFooter.displayName = 'CardFooter';
 
 export { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle };
-
